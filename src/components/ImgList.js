60190a1a6365035407c5adda77b8ad58ebc547a7
najibean <najibullah89@gmail.com>
2021-02-25 23:42:49 +0700

    add reactstrap, bootstrap

--- src/components/ImgList.js
+++ src/components/ImgList.js
@@ -1,40 +1,28 @@
 import React from 'react'
 import '../style/ImgList.css'
+import { Container, Row, Col } from 'reactstrap'
 
 const ImgList = (props) => {
-    // console.log(props)
     return (
-        <div className="container">
-            <div className="row">
+        <Container>
+            <Row>
                 {props.images.map(image => {
                     let srcPath = 'https://farm' + image.farm + '.staticflickr.com/' + image.server + '/' + image.id + '_' + image.secret + '.jpg';
 
                     return (
-                        <div key={image.id} className="col-md-4" style={{ marginBottom: "2rem" }} >
-                            <div className="imageList__container">
-                                <img className="imageList__image" src={srcPath} alt={image.farm} />
+                        <Col sm="4">
+                            <div key={image.id} style={{ marginBottom: "2rem" }} >
+                                <div className="imageList__container">
+                                    <img className="imageList__image" src={srcPath} alt={image.farm} />
+                                </div>
                             </div>
-                        </div>
+
+                        </Col>
                     )
                 })}
-            </div>
-        </div>
+            </Row>
+        </Container>
     )
 }
 
 export default ImgList
-
-
-// {/* nama attribute di app.js adalah "images" */}
-// { props.images.map(image => {
-//     // key={image.id} adalah request dari react nya yang mengharuskan key unique. dan ditempatkan di parents
-//     // return <p key={image.id}>{image.tags}</p>
-
-//     return (
-//         <div key={image.id}>
-//             <img src={image.largeImageURL} alt={image.tags} />
-//             <button>Search</button>
-//         </div>
-//     )
-
-// })}
