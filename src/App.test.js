08810b469ea1495baa160c0e69758a079b69679b
najibean <najibullah89@gmail.com>
2021-02-26 22:56:46 +0700

    update, unsuccessful testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
-import { render, screen } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
+import ReactDOM from 'react-dom'
 import App from './App';
+import ImgSearch from './components/ImgSearch'
+import ImgList from './components/ImgList'
+import "@testing-library/jest-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+afterEach(cleanup)
+
+it('App renders without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<App />, div)
+})
+
+it('ImgSearch renders without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<ImgSearch />, div)
+})
+
+// it('ImgList renders without crashing', () => {
+//   const div = document.createElement('div')
+//   ReactDOM.render(<ImgList />, div)
+// })
+
+it('Button in ImgSearch rendered correctly', () => {
+  const { getByTestId } = render(<ImgSearch />)
+  expect(getByTestId('buttonImgSearch')).toHaveTextContent("Search")
+})
--- src/components/ImgSearch.js
+++ src/components/ImgSearch.js
@@ -7,7 +7,7 @@ const ImgSearch = (props) => {
             {/* onSubmit adalah built-in dari react attribut */}
             <form onSubmit={props.handleRequest} className="imageSearch__form" >
                 <input type="text" autoComplete="off" name="searchValue" placeholder="Search for images..." />&nbsp;
-                <button>Search</button>
+                <button data-testid="buttonImgSearch" >Search</button>
             </form>
         </div>
     )
